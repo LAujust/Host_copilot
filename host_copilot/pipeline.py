207ef5063f40046cf4d507dfed7f4a8e1601aa33
梁润铎 <loyalaujust@outlook.com>
2026-07-23 13:19:45 +0800

    fix bug of pipeline.run

--- host_copilot/pipeline.py
+++ host_copilot/pipeline.py
@@ -35,46 +35,50 @@ class HostPipeline:
         fov = (3*self.r_arcsec/3600)**2
 
         if self.quick:
-            cat_table = Table.from_pandas(self.galaxy_finder.reglade_df)
-            #Only REGLADE catalog
-            print(f"")
-            cat_table['R1'] = cat_table['R1']*u.arcsec
-            cat_table['R2'] = cat_table['R2']*u.arcsec
-            cat_table['PA'] = cat_table['PA']*u.deg
-            cat_table = cat_table[cat_table['z']<self.zcutout]
-            cat_table['sep'] = self.pos.separation(SkyCoord(cat_table['RAJ2000'], cat_table['DEJ2000'], unit='deg')).arcsec * u.arcsec
-            
-            if len(cat_table) == 0:
-                print("No galaxies found in the REGLADE catalog within the specified redshift cut.")
-                return None, None
-            
+            if self.galaxy_finder.reglade_df:
+                cat_table = Table.from_pandas(self.galaxy_finder.reglade_df)
+                #Only REGLADE catalog
+                print(f"")
+                cat_table['R1'] = cat_table['R1']*u.arcsec
+                cat_table['R2'] = cat_table['R2']*u.arcsec
+                cat_table['PA'] = cat_table['PA']*u.deg
+                cat_table = cat_table[cat_table['z']<self.zcutout]
+                cat_table['sep'] = self.pos.separation(SkyCoord(cat_table['RAJ2000'], cat_table['DEJ2000'], unit='deg')).arcsec * u.arcsec
+                
+                if len(cat_table) == 0:
+                    print("No galaxies found in the REGLADE catalog within the specified redshift cut.")
+                    return None, None
+                
+                else:
+                    
+                    print(f"Found {len(cat_table)} galaxies in the REGLADE catalog within the specified redshift cut (z<{self.zcutout}) and radius r={self.r_arcsec} arcsec.")
+                    print('-'*50)
+                    for row in cat_table:
+                        print(f"Galaxy: {row['Name']}, z={row['z']:.3f}, sep={row['sep']:.2f}")
+                
+                    aladin = Aladin(fov=fov, target=self.pos ,survey='CDS/P/PanSTARRS/DR1/color-z-zg-g')
+                    
+                    #Add Catalog
+                    aladin.add_table(cat_table,
+                                    shape=EllipseError(
+                                    maj_axis="R1",
+                                    min_axis="R2",
+                                    angle="PA",
+                                    default_shape="cross"),
+                                    color="cyan",
+                                    )
+
+
+                    #Add Source
+                    circle = CircleSkyRegion(
+                        center=self.pos, radius=Angle(self.r_arcsec, "arcsec"), visual={"edgecolor": "yellow",'linestyle':'dashed'}
+                    )
+                    aladin.add_graphic_overlay_from_region([circle])
+                    
+                    return aladin, cat_table
             else:
-                
-                print(f"Found {len(cat_table)} galaxies in the REGLADE catalog within the specified redshift cut (z<{self.zcutout}) and radius r={self.r_arcsec} arcsec.")
-                print('-'*50)
-                for row in cat_table:
-                    print(f"Galaxy: {row['Name']}, z={row['z']:.3f}, sep={row['sep']:.2f}")
-            
-                aladin = Aladin(fov=fov, target=self.pos ,survey='CDS/P/PanSTARRS/DR1/color-z-zg-g')
-                
-                #Add Catalog
-                aladin.add_table(cat_table,
-                                shape=EllipseError(
-                                maj_axis="R1",
-                                min_axis="R2",
-                                angle="PA",
-                                default_shape="cross"),
-                                color="cyan",
-                                )
-
-
-                #Add Source
-                circle = CircleSkyRegion(
-                    center=self.pos, radius=Angle(self.r_arcsec, "arcsec"), visual={"edgecolor": "yellow",'linestyle':'dashed'}
-                )
-                aladin.add_graphic_overlay_from_region([circle])
-                
-                return aladin, cat_table
+                print("No galaxies found in the REGLADE catalog.")
+                return None, None
         
         
         
@@ -88,8 +92,8 @@ class HostPipeline:
             # Step 1: Find galaxies in the vicinity
             print("Searching for galaxies...")
             self.galaxy_finder.run(quick=self.quick)
-            aladin = self.filter_and_visualize()
-            return aladin
+            aladin, cat_table = self.filter_and_visualize()
+            return aladin, cat_table
 
             # # Step 2: Get cutout images
             # print("Retrieving cutout images...")
